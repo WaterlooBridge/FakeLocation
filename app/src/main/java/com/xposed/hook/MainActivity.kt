0eb9bbbe99dc3f1aef629de4bcdc8c06f87b1ba4
Mikasa <zhenlin1993@gmail.com>
2022-08-27 23:55:05 +0800

    Feature: support search for apps

--- app/src/main/java/com/xposed/hook/MainActivity.kt
+++ app/src/main/java/com/xposed/hook/MainActivity.kt
@@ -2,22 +2,26 @@ package com.xposed.hook
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
@@ -33,21 +37,36 @@ import kotlinx.coroutines.launch
  */
 class MainActivity : AppCompatActivity() {
 
+    private var appList by mutableStateOf(emptyList<AppInfo>())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent { AppScaffold(appList) }
         lifecycleScope.launch {
-            val list = AppHelper.getAppList()
-            setContent { AppList(list) }
+            appList = AppHelper.getAppList()
+        }
+    }
+
+    @Composable
+    fun AppScaffold(list: List<AppInfo>) {
+        AppTheme {
+            Column {
+                var textState by remember { mutableStateOf(TextFieldValue()) }
+                UserInputText(onTextChanged = {
+                    textState = it
+                }, textFieldValue = textState)
+                AppList(if (textState.text.isNotEmpty()) list.filter { info ->
+                    info.title.contains(textState.text, true)
+                } else list)
+            }
         }
     }
 
     @Composable
     fun AppList(list: List<AppInfo>) {
-        AppTheme {
-            LazyColumn {
-                items(list) { item ->
-                    AppItem(item)
-                }
+        LazyColumn {
+            items(list) { item ->
+                AppItem(item)
             }
         }
     }
@@ -70,19 +89,64 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
+    @Composable
+    private fun UserInputText(
+        keyboardType: KeyboardType = KeyboardType.Text,
+        onTextChanged: (TextFieldValue) -> Unit,
+        textFieldValue: TextFieldValue
+    ) {
+        Surface {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .height(64.dp)
+                        .weight(1f)
+                        .align(Alignment.Bottom)
+                ) {
+                    BasicTextField(
+                        value = textFieldValue,
+                        onValueChange = { onTextChanged(it) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 32.dp)
+                            .align(Alignment.CenterStart),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = keyboardType,
+                            imeAction = ImeAction.Search
+                        ),
+                        maxLines = 1,
+                        cursorBrush = SolidColor(LocalContentColor.current),
+                        textStyle = LocalTextStyle.current.copy(color = LocalContentColor.current)
+                    )
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        return if (id == R.id.item_luck_money) {
-            startActivity(Intent(this, LuckMoneySetting::class.java))
-            true
-        } else {
-            super.onOptionsItemSelected(item)
+                    val disableContentColor = MaterialTheme.colors.onSurface
+                    if (textFieldValue.text.isEmpty()) {
+                        Text(
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                                .padding(start = 32.dp),
+                            text = stringResource(id = R.string.search_app),
+                            style = MaterialTheme.typography.body1.copy(color = disableContentColor)
+                        )
+                    }
+                }
+                IconButton(onClick = {
+                    startActivity(
+                        Intent(this@MainActivity, LuckMoneySetting::class.java)
+                    )
+                }) {
+                    Icon(
+                        Icons.Filled.MoreVert,
+                        contentDescription = stringResource(R.string.wechat_hook)
+                    )
+                }
+            }
         }
     }
 }
