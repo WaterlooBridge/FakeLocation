91975a7ebc1eb6b0b056af289bcd7550b902186d
Mikasa <zhenlin1993@gmail.com>
2023-07-01 16:35:25 +0800

    Feature: support 3G network

--- app/src/main/java/com/xposed/hook/location/LocationHook.java
+++ app/src/main/java/com/xposed/hook/location/LocationHook.java
@@ -3,14 +3,17 @@ package com.xposed.hook.location;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.telephony.CellIdentityGsm;
 import android.telephony.CellIdentityLte;
 import android.telephony.CellIdentityNr;
+import android.telephony.CellIdentityTdscdma;
+import android.telephony.CellIdentityWcdma;
+import android.telephony.TelephonyManager;
 import android.telephony.gsm.GsmCellLocation;
 import android.util.Log;
-
-import com.xposed.hook.config.Constants;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -36,12 +39,10 @@ public class LocationHook {
         LocationConfig.setLatitude(latitude);
         LocationConfig.setLongitude(longitude);
 
-        hookMethod("android.net.wifi.WifiManager", mLpp.classLoader, "getScanResults", new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult(Collections.emptyList());
-            }
-        });
+        hookMethod(WifiManager.class, "getScanResults", XC_MethodReplacement.returnConstant(Collections.emptyList()));
+        hookMethod(WifiInfo.class, "getMacAddress", XC_MethodReplacement.returnConstant("02:00:00:00:00:00"));
+        hookMethod(WifiInfo.class, "getSSID", XC_MethodReplacement.returnConstant("<unknown ssid>"));
+        hookMethod(WifiInfo.class, "getBSSID", XC_MethodReplacement.returnConstant("02:00:00:00:00:00"));
 
         hookMethods("android.location.LocationManager", "requestLocationUpdates", new XC_MethodHook() {
             @Override
@@ -64,49 +65,11 @@ public class LocationHook {
         hookMethods("android.location.LocationManager", "getLastKnownLocation", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Location l = (Location) param.getResult();
-                if (l != null) {
-                    LocationHandler.updateLocation(l, LocationConfig.getLatitude(), LocationConfig.getLongitude());
-                    param.setResult(l);
-                } else
+                Location loc = (Location) param.getResult();
+                if (loc != null)
+                    LocationHandler.updateLocation(loc, LocationConfig.getLatitude(), LocationConfig.getLongitude());
+                else
                     param.setResult(LocationHandler.createLocation(LocationConfig.getLatitude(), LocationConfig.getLongitude()));
-            }
-        });
-
-        hookMethod("android.net.wifi.WifiInfo", mLpp.classLoader, "getMacAddress", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult("00-00-00-00-00-00-00-E0");
-            }
-        });
-
-        hookMethod("android.net.wifi.WifiInfo", mLpp.classLoader, "getSSID", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult(null);
-            }
-        });
-
-        hookMethod("android.net.wifi.WifiInfo", mLpp.classLoader, "getBSSID", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult("00:00:00:00:00:00");
-            }
-        });
-
-        hookMethods("android.telephony.TelephonyManager", "getSimState", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                if (lac == Constants.DEFAULT_LAC && cid == Constants.DEFAULT_CID)
-                    param.setResult(0);
-            }
-        });
-
-        hookMethod("android.telephony.TelephonyManager", mLpp.classLoader, "getNeighboringCellInfo", new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                if (lac == Constants.DEFAULT_LAC && cid == Constants.DEFAULT_CID)
-                    param.setResult(null);
             }
         });
 
@@ -132,17 +95,35 @@ public class LocationHook {
             }
         });
 
-        hookMethod(GsmCellLocation.class, "getLac", XC_MethodReplacement.returnConstant((int) lac));
-        hookMethod(GsmCellLocation.class, "getCid", XC_MethodReplacement.returnConstant((int) cid));
+        hookMethod(TelephonyManager.class, "getNeighboringCellInfo", XC_MethodReplacement.returnConstant(null));
 
-        hookMethod(CellIdentityGsm.class, "getLac", XC_MethodReplacement.returnConstant((int) lac));
-        hookMethod(CellIdentityGsm.class, "getCid", XC_MethodReplacement.returnConstant((int) cid));
+        int ac = (int) lac;
+        int ci = cid > Integer.MAX_VALUE ? -1 : (int) cid;
 
-        hookMethod(CellIdentityLte.class, "getTac", XC_MethodReplacement.returnConstant((int) lac));
-        hookMethod(CellIdentityLte.class, "getCi", XC_MethodReplacement.returnConstant((int) cid));
+        hookMethod(GsmCellLocation.class, "getLac", XC_MethodReplacement.returnConstant(ac));
+        hookMethod(GsmCellLocation.class, "getCid", XC_MethodReplacement.returnConstant(ci));
+
+        // 2G
+        hookMethod(CellIdentityGsm.class, "getLac", XC_MethodReplacement.returnConstant(ac));
+        hookMethod(CellIdentityGsm.class, "getCid", XC_MethodReplacement.returnConstant(ci));
+
+        // 3G
+        hookMethod(CellIdentityWcdma.class, "getLac", XC_MethodReplacement.returnConstant(ac));
+        hookMethod(CellIdentityWcdma.class, "getCid", XC_MethodReplacement.returnConstant(ci));
+
+        // 4G
+        hookMethod(CellIdentityLte.class, "getTac", XC_MethodReplacement.returnConstant(ac));
+        hookMethod(CellIdentityLte.class, "getCi", XC_MethodReplacement.returnConstant(ci));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // 3G
+            hookMethod(CellIdentityTdscdma.class, "getLac", XC_MethodReplacement.returnConstant(ac));
+            hookMethod(CellIdentityTdscdma.class, "getCid", XC_MethodReplacement.returnConstant(ci));
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            hookMethod(CellIdentityNr.class, "getTac", XC_MethodReplacement.returnConstant((int) lac));
+            // 5G
+            hookMethod(CellIdentityNr.class, "getTac", XC_MethodReplacement.returnConstant(ac));
             hookMethod(CellIdentityNr.class, "getNci", XC_MethodReplacement.returnConstant(cid));
         }
     }
